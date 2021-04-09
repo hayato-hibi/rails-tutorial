16eb1760ed19fa771a3d30a929ef5cbf9fe5b5bd
hayto <hayatocats@gmail.com>
2021-04-09 13:06:53 +0900

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hole, mondo!"
-  end
-
-  def goodbye
-    render html: "goodbye, world"
+    render html: "Hello World!"
   end
 end
